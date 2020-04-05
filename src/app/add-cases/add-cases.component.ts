51e717509c57e678e75ecd53985028d3de7f42ba
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2020-04-05 05:03:21 +0100

    0.3

--- src/app/add-cases/add-cases.component.ts
+++ src/app/add-cases/add-cases.component.ts
@@ -5,6 +5,13 @@ import { ApiService } from '../api.service';
 import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
 @Component({
   selector: 'app-add-cases',
   templateUrl: './add-cases.component.html',
@@ -12,9 +19,43 @@ import { ErrorStateMatcher } from '@angular/material/core';
 })
 export class AddCasesComponent implements OnInit {
 
-  constructor() { }
+  casesForm: FormGroup;
+  name = '';
+  gender = '';
+  age: number = null;
+  address = '';
+  city = '';
+  country = '';
+  status = '';
+  statusList = ['Positive', 'Dead', 'Recovered'];
+  genderList = ['Male', 'Female'];
+  isLoadingResults = false;
+  matcher = new MyErrorStateMatcher();
+
+
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.casesForm = this.formBuilder.group({
+      name : [null, Validators.required],
+      gender : [null, Validators.required],
+      age : [null, Validators.required],
+      address : [null, Validators.required],
+      city : [null, Validators.required],
+      country : [null, Validators.required],
+      status : [null, Validators.required]
+    });
   }
-
+  onFormSubmit() {
+    this.isLoadingResults = true;
+    this.api.addCases(this.casesForm.value)
+      .subscribe((res: any) => {
+          const id = res._id;
+          this.isLoadingResults = false;
+          this.router.navigate(['/cases-details', id]);
+        }, (err: any) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        });
+  }
 }
